73ec83842bbbe112f0f5830dcd40b4074fe63227
FazioNico <contact@nicolasfazio.ch>
2026-02-12 09:33:47 +0100

    add header

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Vite</h1>
-  </div>
-`
+  <header>
+    <span>Vite + Vanilla</span>
+  </header>
+  <h1>Main content</h1>
+`;
 
